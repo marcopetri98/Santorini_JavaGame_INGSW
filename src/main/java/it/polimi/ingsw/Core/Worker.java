ad8b76bde47014f945065fd327f8b8d4fab1eb8f
aless <10608696@polimi.it>
2020-03-22 13:24:30 +0100

    initial draft of various classes

---
+++ src/main/java/it/polimi/ingsw/Core/Worker.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.Core;
+
+public class Worker {
+    private int level;
+    private String color;
+
+    public Worker(String color){
+        this.color = color;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
