7efe76fb4867b264db3af208bf57576ed433004b
Marco Petri <marco.petri@mail.polimi.it>
2020-05-13 19:19:34 +0200

    Improved MapTest to gain 100% statement coverage

--- src/test/java/it/polimi/ingsw/core/MapTest.java
+++ src/test/java/it/polimi/ingsw/core/MapTest.java
@@ -12,28 +12,52 @@ import static org.junit.Assert.*;
 
 public class MapTest {
 	private Map map;
-	private List<List<Cell>> cells;
-	int x,y;
 
 	@Before
 	public void testSetup(){
 		map = new Map();
-		cells = new ArrayList<>();
-		for (int i = 0; i < Constants.MAP_SIDE; i++) {
-			cells.add(new ArrayList<Cell>());
-			for (int j = 0; j < Constants.MAP_SIDE; j++) {
-				cells.get(i).add(new Cell(map));
-			}
-		}
-
 	}
 
 	@Test
 	public void getCellTest(){
-		x=0; y=0;
-		Cell cella = map.getCell(x, y);
-		assertEquals(map.getX(cella), 0);
+		int x=0, y=0;
+		Cell cell = map.getCell(x, y);
+		assertEquals(map.getX(cell), 0);
+		assertEquals(map.getY(cell), 0);
+	}
 
-		//assertEquals(cells.get(x).get(y), map.getCell(x,y));
+	@Test
+	public void equals() {
+		Map map2 = new Map();
+		Map map3 = new Map();
+
+		// reflexive property
+		assertTrue(map.equals(map));
+		// symmetric property
+		assertTrue(map.equals(map2));
+		assertTrue(map2.equals(map2));
+		// transitive property
+		assertTrue(map2.equals(map3));
+		assertTrue(map.equals(map3));
+
+		// check inequality
+		map2.getCell(0,0).getBuilding().incrementLevel();
+		assertFalse(map.equals(map2));
+
+		// check on the same type
+		assertFalse(map.equals("I'm an ugly and sad string :("));
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void illegalArgumentToGet() {
+		Map map2 = new Map();
+
+		try {
+			map.getX(map2.getCell(0,0));
+			fail();
+		} catch (IllegalArgumentException e) {
+			map.getY(map2.getCell(0,0));
+			fail();
+		}
 	}
 }
