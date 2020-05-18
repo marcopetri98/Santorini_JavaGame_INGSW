f7fa52edd7eaa7c49ae530ecb340dc1f51bc0f9d
g-picc <61702192+g-picc@users.noreply.github.com>
2020-05-18 08:58:25 +0200

    GUI: loadingScene v2

--- src/main/java/it/polimi/ingsw/ui/gui/controller/LoadingSceneController.java
+++ src/main/java/it/polimi/ingsw/ui/gui/controller/LoadingSceneController.java
@@ -11,24 +11,46 @@ import javafx.util.Duration;
 public class LoadingSceneController {
 
 	@FXML
-	private ImageView icon_zeus;
+	private ImageView icon;
 
 	@FXML
 	private ImageView button_exit;
 
+	@FXML
+	private ImageView loading_background;
+
 	Image buttonExitPressed = new Image("/img/home_exit_btn_pressed.png");
 	Image buttonExit = new Image("/img/home_exit_btn.png");
 
-	/**
-	 * moving icon_zeus
-	 */
+	Image logoGods = new Image("/img/loading_godspng.png");
+	Image logoStarter = new Image("/img/loading_starterpng.png");
+	Image logoLoading = new Image("/img/loading_zeuspng.png");
+	Image loading = new Image("/img/loading_background.png");
+	Image loadingGods = new Image("/img/loading_gods_background.png");
+	Image loadingStarter = new Image("/img/loading_starter_background.png");
+
+
+
+
 	public void initialize(){
+		loading_background.setImage(phase());
 		PathTransition transition = new PathTransition();
-		transition.setNode(icon_zeus);
+		icon.setImage(phaseLogo());
+		transition.setNode(icon);
 		transition.setDuration(Duration.millis(3500));
-		transition.setPath(new Circle(327, 230, 12)); //x,y,radius (pixels)
+		transition.setPath(new Circle(630, 362, 12)); //x,y,radius (pixels)
 		transition.setCycleCount(PathTransition.INDEFINITE);
 		transition.play();
+	}
+
+	private Image phase(){
+		//TODO:...
+		return loadingGods;
+	}
+
+	private Image phaseLogo(){
+		//TODO:...
+		return logoGods;
 	}
 
 	public void mousePressedExit(MouseEvent mouseEvent) {
